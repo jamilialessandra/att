d798ef33d830c78c35ed9a389084fb0513a10e59
Your Namejamilialessandra <jamilisilvah54@gmail.com>
2023-09-25 11:27:51 -0300

    mudei

---
+++ Ninho/database/Produto.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+const Produto = connection
+.define('produto', {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    descricao : {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Produto.sync({force:false});
+module.exports = Produto;
